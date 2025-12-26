3f90641c99cae345a426f8fae227bac93dc88b23
jorgecif <jorgecif@gmail.com>
2025-12-26 10:19:29 -0500

    update

--- app.py
+++ app.py
@@ -820,7 +820,7 @@ if selected == "Cajón":
             st.write("Texto codificado:")
             #st.write(text_output)
             #st.image(image,width=None, use_container_width=True )
-            st.image(text_output, width=40, use_container_width=False)
+            st.image(text_output, width=40)
 
 if selected == "Palitos":
     st.title(f"Clave {selected}")
